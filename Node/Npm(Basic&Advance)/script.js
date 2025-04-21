c0adb43a84f356b16fc8d298ce76952532dadc3f
HarshUpadhyay518 <harshupadhyay518@gmail.com>
2025-04-21 23:24:36 +0530

    Initial Commit

---
+++ Node/Npm(Basic&Advance)/script.js
@@ -0,0 +1,10 @@
+//  Installing and Uninstalling
+
+// install -> npm i packageKaNaam
+// uninstall -> npm uninstall packagename
+
+// dependencies -> package kisi package per depend hai 
+
+// devdependencies -> dev mein kaam aata hai aur jab project deploy ho jata hai toh phir inka use nhi hota 
+
+// scripts -> 
---
+++ Node/script.js
@@ -0,0 +1,56 @@
+const { log } = require('console');
+const fs = require ('fs');
+
+// ----------- Write in file ----------------
+
+// fs.writeFile("hey.xtt","hello kaise ho",function(err){
+//     if(err) console.log(err);
+//     else console.log("done");
+    
+// })
+
+// ----------- Append in file ----------------
+
+// fs.appendFile("hey.xtt","mein toh acha",function(err){
+//     if(err) console.log(err);
+//     else console.log("done");
+    
+// })
+
+// ------------ Rename file -------------
+
+// fs.rename("hey.xtt","hello.txt",function (err) {
+//     if (err) {
+//         console.error(err);
+//     }
+//     else console.log("done");
+// })
+
+// ------------ Copy File -----------
+
+// fs.copyFile("hello.txt", "copy_of_hello.txt", function(err) {
+//     if(err) log(err);
+//     else log("done");
+//})
+
+// ------------ Unlink File ---------
+
+// fs.unlink("hello.txt",function(err){
+//     if(err) console.error(err);
+//     else console.log("removed");
+// })
+
+// ------------- rmdir -------------
+
+// fs.rm("./copy", {recursive: true},function(err){
+//     if(err) console.error(err);
+//     else console.log("removed")
+// })
+
+// -------- Read file ----------------
+// fs.readFile("./copy","utf8",function(err,data){
+//     if(err) return console.error(err);
+//     else console.log(data);
+// })
+
+
